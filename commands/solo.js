7a248e7386b758c5b271932ae0cfa8a0ad0a9e8d
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-19 18:59:55 +0200

    Update solo.js

--- commands/solo.js
+++ commands/solo.js
@@ -28,7 +28,7 @@ module.exports.run = async (blaki, message, args) => {
     .setURL('https://discord.gg/u9W8euF')
     .addField("**HASŁO:**", `**${pass}**`, true)
     .addField("**HOST:**", `${message.author}`, true)
-    .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4 + `\n`+ zasady5 + `\n`+ zasady6)
+    .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4 + `\n`+ zasady5 + `\n`+`\n`+ zasady6)
     .setTimestamp(message.createdAt)
     .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/eBDl1P5.jpg");
     message.channel.send(everyone.toString());
