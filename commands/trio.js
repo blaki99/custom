645573bd94f7ba643ce08b5cda8565ec574e2c12
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-19 18:45:53 +0200

    Update trio.js

--- commands/trio.js
+++ commands/trio.js
@@ -31,7 +31,7 @@ module.exports.run = async (blaki, message, args) => {
     .addField("**HOST:**", `${message.author}`, true)
     .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4 + `\n`+ zasady5 + `\n`+ zasady6 + `\n`+ `\n`+ zasady7)
     .setTimestamp(message.createdAt)
-    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/3Q7TQyy.png");
+    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/eBDl1P5.jpg");
     message.channel.send(everyone.toString());
     message.channel.send(customEmbed).then(function (message) {
         message.react(react)
