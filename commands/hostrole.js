7fb4e1d4bd0c4556faf887e21ad3eed55c2e9bf6
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-20 23:37:26 +0200

    Create hostrole.js

---
+++ commands/hostrole.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (custom, message, args) => {
+
+  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Nie posiadasz uprawnień.");
+  if(!args[0] || args[0 == "help"]) return message.reply("Użycie: -host <nazwa roli bez @>");
+
+  let roles = JSON.parse(fs.readFileSync("./roles.json", "utf8"));
+
+  roles[message.guild.id] = {
+    roles: args[0]
+  };
+
+  fs.writeFile("./roles.json", JSON.stringify(roles), (err) => {
+  });
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("#FF0000")
+  .setTitle("Rola została ustawiona!")
+  .setDescription(`Ustawiono Hosta na ${args[0]}`);
+
+  message.channel.send(sEmbed);
+
+}
+
+module.exports.help = {
+  name: "role"
+}
