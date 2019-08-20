a7ef49b5328e6c567b18159d723809ca927b3ea5
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-20 08:16:36 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,17 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (blaki, message, args) => {
     
-    let HOST = message.guild.roles.find("name", "Host Customy");
     message.delete().catch(O_o=>{});
     if(!message.member.roles.has(HOST.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
     
-    let TrioEmbed = new Discord.RichEmbed()
+    let HelpEmbed = new Discord.RichEmbed()
     .setColor("#FF0000")
     .addField("__**KOMENDY:**__", "-solo" + `\n` + "-duo" + `\n` + "-trio" + `\n` + "-msg, -msgr, -msgb, -msgo")
     .setTimestamp(message.createdAt)
     .setFooter("Dostępne Komendy", "https://i.imgur.com/3Q7TQyy.png");
 
-    message.channel.send(TrioEmbed);
+    message.channel.send(HelpEmbed);
 }
 
 module.exports.help = {
