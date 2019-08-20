a2c77c7d23d04396edd40cccaab8fd6390321733
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-20 22:41:16 +0200

    Update msg.js

--- commands/msg.js
+++ commands/msg.js
@@ -2,20 +2,17 @@ const Discord = require("discord.js");
 
 module.exports.run = async (custom, message, args) => {
 
-    let host = message.guild.roles.find("name", "HOST");
-
     let msg = (args[0]);
     let msg2 = args.slice(1).join(' ')
     const ramka = "```"
-    const ramka2 = "```"
 
-    if(!message.member.roles.has(host.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
+    if(!message.member.roles.find(r => r.name === "HOST")) return message.reply("Ooops, nie posiadasz uprawnień!");
     if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **!msg <wiadomość>**_ ❌")
     message.delete();
     let msgEmbed = new Discord.RichEmbed()
     .setColor("#6B7074")
     .addField("__**INFORMACJA:**__", `${ramka}
-${msg} ${msg2}${ramka2}`)
+${msg} ${msg2}${ramka}`)
     .addField("__**OD:**__", `${message.author}`)
     .setTimestamp(message.createdAt)
     .setFooter("Nowa Informacja", "https://i.imgur.com/9A72yKJ_d.jpg");
