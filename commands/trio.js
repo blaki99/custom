fb8523e39e12525af4546d07fb2110e22ea75691
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-20 23:11:21 +0200

    Update trio.js

--- commands/trio.js
+++ commands/trio.js
@@ -13,7 +13,9 @@ module.exports.run = async (custom, message, args) => {
     const zasady6 = "**NIESTOSOWANIE SIĘ DO POWYŻSZYCH ZASAD BĘDZIE KARANE BANEM !**"
     const react = '✅'
   
-    if(!message.member.roles.find(r => r.name === "HOST")) return message.reply("Ooops, nie posiadasz uprawnień!");
+    let content = message.member.roles.findtoLowerCase()
+
+    if(!message.member.roles.some(role => role.name === 'HOST')) return message.reply("Ooops, nie posiadasz uprawnień!");
     if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **-trio hasło **_ ❌").then(() =>
     {
         message.channel.send("❌ _**Utwórz hasło, które nie będzie za krótkie!**_ ❌");
