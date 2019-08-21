6abb8e7c76c11bda8a8930c088e548b9569e610c
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-21 14:59:39 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (custom, message, args) => {
     .setColor("#FF0000")
     .addField("__**KOMENDY:**__", `>>> solo` + `\n` + `duo` + `\n` + `trio` + `\n` + `msg`)
     .setTimestamp(message.createdAt)
-    .setFooter("Dostępne Komendy", message.custom.avatarURL);
+    .setFooter("Dostępne Komendy", "https://i.imgur.com/6Ew0Bza.png");
     message.channel.send(HelpEmbed);
 }
 
