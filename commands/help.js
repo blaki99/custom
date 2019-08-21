277f36c7b1a404deec6b7d4f94449f4db438bc7e
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-21 11:21:54 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,18 +5,9 @@ module.exports.run = async (custom, message, args) => {
     
     message.delete().catch(O_o=>{});
 
-    let prefixes = JSON.parse(fs.readFileSync("../prefixes.json", "utf8"));
-    if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: CustomConfig.prefix
-      };
-    }
-
-    let prefix = prefixes[message.guild.id].prefixes;
-
     let HelpEmbed = new Discord.RichEmbed()
     .setColor("#FF0000")
-    .addField("__**KOMENDY:**__", `> ${prefix}solo` + `\n` + `> ${prefix}duo` + `\n` + `> ${prefix}trio` + `\n` + `> ${prefix}msg`)
+    .addField("__**KOMENDY:**__", `> solo` + `\n` + `> duo` + `\n` + `> trio` + `\n` + `> msg`)
     .setTimestamp(message.createdAt)
     .setFooter("Dostępne Komendy", "https://i.imgur.com/9A72yKJ_d.jpg");
     message.channel.send(HelpEmbed);
