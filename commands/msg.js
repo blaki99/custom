c8153ee1b1d616cde3b34ba45f863365ba19092c
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-09-21 18:44:25 +0200

    Update msg.js

--- commands/msg.js
+++ commands/msg.js
@@ -3,7 +3,7 @@ const config = require("../blakiconfig.json");
 
 module.exports.run = async (custom, message, args) => {
 
-    let host = message.guild.roles.find("name", "HOST");
+    let host = message.guild.roles.find("name", "HOST 💜");
 
     let msg = (args[0]);
     let msg2 = args.slice(1).join(' ')
